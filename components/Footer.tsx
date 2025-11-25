6989d9cf1a4d544a845a0bf90afe5eab3cab38c2
Your Name <irene.germani@kcl.ac.uk>
2025-11-25 23:02:20 +0000

    Update header footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,20 +2,19 @@ import personalInfo from './data/personalInfo.json';
 import SupportingBox from './SupportingBox';
 
 const Footer = (): JSX.Element => {
-    return (
-        <footer>
-            <div className="mb-9 pb-0 pl-6 max-w-4xl flex items-center justify-center text-sm opacity-60 m-auto pt-20 pr-5">
-                <SupportingBox />
-            </div>
-            
-            <p className="text-center text-sm opacity-40 dark:opacity-50 pb-4 pt-4">
-                Built with <i>Next.js, TailwindCSS</i>
-                {personalInfo.love && (
-                    <> and <a href={personalInfo.love} target="_blank" rel="noopener noreferrer">💛</a></>
-                )}
-            </p>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="max-w-4xl w-full mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center text-sm opacity-60 pt-20 mb-9">
+        <SupportingBox />
+      </div>
+      <p className="text-center text-sm opacity-40 dark:opacity-50 pb-4 pt-4">
+        Built with <i>Next.js, TailwindCSS</i>
+        {personalInfo.love && (
+          <> and <a href={personalInfo.love} target="_blank" rel="noopener noreferrer">💛</a></>
+        )}
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
